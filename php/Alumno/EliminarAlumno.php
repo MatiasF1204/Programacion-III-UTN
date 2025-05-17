88bbb8e0c612e187390d89ebc71f11d79f684d14
Matías Flores <matiasflores120402@gmail.com>
2025-05-16 22:46:52 -0300

    Se validó EliminarMateria.php

--- php/Alumno/EliminarAlumno.php
+++ php/Alumno/EliminarAlumno.php
@@ -16,19 +16,25 @@ include '../Conexiones.php';
     <div class="contenedor-mensaje">
         <?php
         if (isset($_GET['id'])) {
+            // Obtengo ID del alumno a eliminar
             $id = $_GET['id'];
 
+            // Selecciono a los alumnos que se encuentren en la tabla cursada
             $validar = "SELECT * FROM cursada 
                     WHERE id_alumno = $id
-        ";
+            ";
 
+            // Ejecuto consulta de validación
             $resultado_validar = mysqli_query($connection, $validar);
 
+            // Obtengo la cantidad de alumnos encontrados
             $cantidad = mysqli_num_rows($resultado_validar);
 
+            // Si la cantidad es mayor a cero, es decir, que SI está ese alumno en la tabla cursada:
             if ($cantidad > 0) {
                 echo "No se puede eliminar un alumno que está registrado en una cursada.";
             } else {
+                // En caso de que no esté, se lo elimina
                 $sql = "DELETE FROM alumno WHERE id = $id";
 
                 if (mysqli_query($connection, $sql)) {
--- php/Materia/EliminarMateria.php
+++ php/Materia/EliminarMateria.php
@@ -3,16 +3,25 @@ include '../Conexiones.php';
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM materias 
+$sql_validar = "SELECT * FROM curasada
+                WHERE id_materia = $id";
+
+$resultado_validar = mysqli_query($connection, $sql_validar);
+
+$cantidad = mysqli_num_rows($resultado_validar);
+
+if ($cantidad > 0) {
+    echo "No se puede eliminar una materia que está asociada a una cursada.";
+} else {
+    $sql = "DELETE FROM materias 
         WHERE id = $id";
 
-$respuesta = mysqli_query($connection, $sql);
+    $respuesta = mysqli_query($connection, $sql);
 
-if ($respuesta) {
-    echo "Materia eliminada exitosamente.";
-    echo '<br> <a href="../../index.html">Volver a Inicio</a>';
-} else {
-    echo "Error al eliminar materia: " . mysqli_error($connection);
+    if ($respuesta) {
+        echo "Materia eliminada exitosamente.";
+        echo '<br> <a href="../../index.html">Volver a Inicio</a>';
+    } else {
+        echo "Error al eliminar materia: " . mysqli_error($connection);
+    }
 }
-?>
-
